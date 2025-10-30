76832f11e9b7b56d49ac78a7a8d0eb8881d96516
DESKTOP-O536MOU\PabloSG <pablosuarez2702@gmail.com>
2025-10-30 10:40:57 +0100

    COMMIT DIA 30/10/2025

--- 2ºDAW/DIWEB ,DWEC Y PIDAWE/DWEC/1ºTrimestre/Ejercicios/Unidad 2/JS/a2.2.js
+++
@@ -1,3 +0,0 @@
-document.write("Hola");
-window.alert("Hola alerta js");
-console.log("Hola alerta js");
--- 2ºDAW/DIWEB ,DWEC Y PIDAWE/DWEC/1ºTrimestre/Ejercicios/Unidad 2/JS/a2.4.js
+++
@@ -1,1 +0,0 @@
-console.log('El acceso a la ruta C:\\usuario tarda 1’23”, algo considerado "lento" en la actualidad.');
