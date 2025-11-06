298aef3d3bbdf288cb28ce07e8bd27bf545c198e
DESKTOP-O536MOU\PabloSG <pablosuarez2702@gmail.com>
2025-11-06 14:39:28 +0100

    COMMIT 06/11/2025

---
+++ 2ºDAW/OGS/PHP/actividad5.php
@@ -0,0 +1,11 @@
+<form method="post">
+Nombre: <input type="text" name="nombre"><br>
+Apellidos: <input type="text" name="apellidos"><br>
+Edad: <input type="number" name="edad"><br>
+<input type="submit" value="Mostrar datos" name="mostrar">
+</form>
+
+<?php
+
+
+?>
